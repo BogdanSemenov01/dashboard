8022db2630e4b3512c11f7e3a91961b547ccbd2f
BogdanAburame <my_project2020@inbox.ru>
2023-01-07 22:30:32 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import ProjectsBoard from './components/ProjectsBoard';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 70px;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return  (
+    <BrowserRouter>
+      <AppWrapper>
+        <Routes>
+          <Route path='/' element={<ProjectsBoard />}/>
+          <Route path='/:id' element={<div>Tasks</div>}/>
+        </Routes>
+      </AppWrapper>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ProjectsBoard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProjectsBoard = (props:any) => {
+  return (
+    <div>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ProjectsBoard
